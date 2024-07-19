9afaf914ef7adc5fb2c8b28d5778a2faa3c32337
Swaraj Singh <72075925+swrjsingh@users.noreply.github.com>
2024-07-19 16:58:43 +0530

    Removed exposed key

--- apps/web/app/authentication-provider.tsx
+++ apps/web/app/authentication-provider.tsx
@@ -22,11 +22,7 @@ export function AuthenticationProvider({
   const { resolvedTheme } = useTheme();
   return (
     <ClerkProvider
-      publishableKey={
-        process.env.NODE_ENV === "development"
-          ? "pk_test_bmF0aW9uYWwtbW9zcXVpdG8tMjUuY2xlcmsuYWNjb3VudHMuZGV2JA"
-          : process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
-      }
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
       appearance={{
         baseTheme: resolvedTheme === "dark" ? dark : undefined,
         variables: {
