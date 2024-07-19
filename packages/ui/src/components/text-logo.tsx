3fc2cf5d5349e3da0cf5bae95fec48c533f5b5bc
Swaraj Singh <72075925+swrjsingh@users.noreply.github.com>
2024-07-19 07:04:18 +0530

    Update text-logo.tsx to include Theta Space Logos

--- packages/ui/src/components/text-logo.tsx
+++ packages/ui/src/components/text-logo.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 export default function TextLogo({
   className,
   isPlus,
@@ -7,12 +9,23 @@ export default function TextLogo({
 }) {
   return (
     <span
-      className={`flex items-center font-medium ${className} hover:opacity-50`}
+      className={`flex items-center ${className} hover:opacity-50`}
     >
-      <span className="lg:hidden">{`ORP${isPlus ? "+" : ""}`}</span>
-      <span className="hidden lg:inline">{`OpenRoleplay${
-        isPlus ? "+" : ""
-      }`}</span>
+      <Image
+        src="/LogoSVG-32x32.svg"
+        alt="Theta Space Logo"
+        width={32}
+        height={32}
+        className="h-8 w-8 sm:hidden"
+      />
+      <Image
+        src="/LogoSVG-250x50.svg"
+        alt="Theta Space Logo"
+        width={250}
+        height={50}
+        className="hidden h-8 w-auto sm:block"
+      />
+      {isPlus && <span className="ml-1 font-medium">+</span>}
     </span>
   );
 }
